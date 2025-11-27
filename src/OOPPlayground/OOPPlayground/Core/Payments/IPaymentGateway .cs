4b34a82748afe46843494811bae0891f4fd9b5e4
mahmoodghanbari <code.mahmood@gmail.com>
2025-11-27 07:41:29 -0800

    feat(payments): add IPaymentGateway and PaymentProcessor base

---
+++ src/OOPPlayground/OOPPlayground/Core/Payments/IPaymentGateway .cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPPlayground.Core.Payments
+{
+    public interface IPaymentGateway
+    {
+        Task<PaymentResult> ChargeAsync(PaymentRequest request);
+    }
+}
---
+++ src/OOPPlayground/OOPPlayground/Core/Payments/PaymentProcessor.cs
@@ -0,0 +1,26 @@
+﻿using OOPPlayground.Core.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPPlayground.Core.Payments
+{
+    public abstract class PaymentProcessor : IPaymentGateway
+    {
+        protected readonly ILogger _logger;
+
+        protected PaymentProcessor(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public abstract Task<PaymentResult> ChargeAsync(PaymentRequest request);
+
+        protected void Log(decimal amount, string gateway)
+        {
+            _logger.Log($"Payment of {amount:C} processed by {gateway}");
+        }
+    }
+}
