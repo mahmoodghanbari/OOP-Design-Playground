e33b04f46141399df0f3a06fe8080aac7e5f1e7e
mahmoodghanbari <code.mahmood@gmail.com>
2025-11-27 07:38:56 -0800

    feat(notification): add INotifier, EmailNotifier and SmsNotifier

---
+++ src/OOPPlayground/OOPPlayground/Core/Notifications/EmailNotifier.cs
@@ -0,0 +1,26 @@
+﻿using OOPPlayground.Core.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPPlayground.Core.Notifications
+{
+    public class EmailNotifier : INotifier
+    {
+        private readonly ILogger _logger;
+
+        public EmailNotifier(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Send(string to, string message)
+        {
+            // Simulate email sending
+            _logger.Log($"Sending EMAIL to {to}: {message}");
+            Console.WriteLine($"[EMAIL] delivered to {to}");
+        }
+    }
+}
---
+++ src/OOPPlayground/OOPPlayground/Core/Notifications/SmsNotifier.cs
@@ -0,0 +1,26 @@
+﻿using OOPPlayground.Core.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPPlayground.Core.Notifications
+{
+    public class SmsNotifier : INotifier
+    {
+        private readonly ILogger _logger;
+
+        public SmsNotifier(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Send(string to, string message)
+        {
+            // Simulate SMS sending
+            _logger.Log($"Sending SMS to {to}: {message}");
+            Console.WriteLine($"[SMS] delivered to {to}");
+        }
+    }
+}
