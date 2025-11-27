dc17c6067bf4412efb6b6eaae7f39de02d036d36
mahmoodghanbari <code.mahmood@gmail.com>
2025-11-27 07:35:29 -0800

    feat(logging): add ILogger and ConsoleLogger implementation

---
+++ src/OOPPlayground/OOPPlayground/Core/Logging/ConsoleLogger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPPlayground.Core.Logging
+{
+    public class ConsoleLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine($"[LOG] {DateTime.Now:yyyy-MM-dd HH:mm:ss}: {message}");
+        }
+    }
+}
